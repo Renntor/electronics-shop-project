69d655b65c89c0121a24b3c9c49989002c9d75f3
Renntor <132259747+Renntor@users.noreply.github.com>
2023-06-06 14:26:24 +0300

    Update phone.py
    removed __add__

--- src/phone.py
+++ src/phone.py
@@ -7,11 +7,6 @@ class Phone(Item):
         super().__init__(name, price, quantity)
         self.__number_of_sim = number_of_sim
 
-    def __add__(self, other):
-        if isinstance(other, Item):
-            return self.quantity + other.quantity
-        return None
-
     def __str__(self):
         return self.name
 
